bb5c2b8d5b9da9d5f06986a1c4826f6d9bcaa8d1
Gaurav Sekhri <gauravsekhri69@gmail.com>
2023-12-30 23:14:40 +0530

    hyperlink modal bug fix

--- components/BlogsModule/BlogEditor/index.tsx
+++ components/BlogsModule/BlogEditor/index.tsx
@@ -188,7 +188,7 @@ const BlogEditor = ({ initialText, onChange }: BlogEditor) => {
             .setLink({ href: val })
             .run();
         }
-        setShowImgDialog(false);
+        closeTextDialog();
       },
     });
   };
